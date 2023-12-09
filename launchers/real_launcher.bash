5af0eb4c8eaf0607ab91822934e1910a78b526df
gnahzom <116391198+gnahzom@users.noreply.github.com>
2023-12-08 19:05:48 -0800

    Update real_launcher.bash

--- launchers/real_launcher.bash
+++ launchers/real_launcher.bash
@@ -1,7 +1,7 @@
 #!/bin/bash; bash"
 #Use this to see all 5 nodes in separate terminals
 
-gnome-terminal -- sh -c "rosrun rosserial_python serial_node.py /dev/ttyACM0; bash"
+gnome-terminal -- sh -c "rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 _baud:=9600; bash"
 gnome-terminal -- sh -c "cd ..; cd computer_vision/; python cv_start.py; bash"
 gnome-terminal -- sh -c "cd ..; cd graphical_user_interface/src/; python gui_node.py; bash"
 gnome-terminal -- sh -c "cd ..; cd guidance_navigation_control/src/; python final_gnc_node.py; bash"
